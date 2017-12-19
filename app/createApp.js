c1a0a68df65170e3ab3bead4e35c6e931eca5661
nikitaskar <n.skargovskii@gmail.com>
2017-12-19 01:26:29 +0100

    line

--- app/createApp.js
+++ app/createApp.js
@@ -3,7 +3,8 @@ import orbitControls from 'orbit-controls'
 import glsl from 'glslify'
 import path from 'path'
 import EffectComposer, { RenderPass, ShaderPass, CopyShader } from 'three-effectcomposer-es6'
-
+import meshLine from 'three.meshline'
+import gsap from 'gsap'
 class createApp {
 	constructor(opt) {
 		this.winWidth = window.innerWidth
@@ -11,17 +12,18 @@ class createApp {
 		this.winRatio = this.winWidth/this.winHeight
 		this.camera = new THREE.PerspectiveCamera( 75,this.winRatio, 0.1, 1000 )
 		this.target = new THREE.Vector3()
-		this.camera.position.z = 10
+		this.camera.position.z = 65
 
 		this.scene = new THREE.Scene();
 
 		this.controls = orbitControls({
 			position : [0,0,0],
-			distance: 10,
-			zoom: false,
+			distance: 65,
+			zoom: true,
 			rotateSpeed: 0.007,
 			damping: 0.05,
 		})
+		this.anim = false;
 		this.uniforms = {
 			viewVector: {
 				type:"v3",
@@ -29,7 +31,14 @@ class createApp {
 			}
 		}
 		
-		this.material = new THREE.ShaderMaterial({
+		console.log(meshLine)
+
+		var geometry = new THREE.Geometry();
+		for( var i = 0; i < 500; i ++) {
+			let phi = Math.acos( -1 + ( 2 * i ) / 500 );
+			let theta = Math.sqrt(500 * Math.PI/3 ) * phi;
+
+			this.material = new THREE.ShaderMaterial({
 			vertexShader:glsl((`uniform vec3 viewVector;
 				varying float intensity;
 				void main() {
@@ -48,11 +57,31 @@ class createApp {
 			uniforms: this.uniforms
 
 		});
-		this.geometry = new THREE.SphereGeometry(2,64,64)
+		this.geometry = new THREE.SphereGeometry(2,8,8)
 		this.mesh = new THREE.Mesh(this.geometry, this.material)
+		this.mesh.position.x = 30 * Math.cos( theta ) * Math.sin( phi );
+		this.mesh.position.y = -30 * Math.cos( phi );
+		this.mesh.position.z = 30 * Math.sin( theta ) * Math.sin( phi )
+		// this.scene.add(this.mesh)
+		
 
+		    var v = new THREE.Vector3( 30 * Math.cos( theta ) * Math.sin( phi ), -30 * Math.cos( phi ), 30 * Math.sin( theta ) * Math.sin( phi ) );
+		    geometry.vertices.push( v );
+		}
 
-		this.scene.add(this.mesh)
+		var line = new meshLine.MeshLine();
+
+		line.setGeometry( geometry, function( p ) { return 1*Math.pow( 4 * p * ( 1 - p ), 1 ); } ); // makes width sinusoidal 
+
+		var material = new meshLine.MeshLineMaterial({
+			lineWidth: 1,
+			visibility: 0,
+			transparent: true,
+		});
+		this.line = new THREE.Mesh( line.geometry, material ); // this syntax could definitely be improved! 
+		this.scene.add( this.line );
+
+		
 
 		console.log(CopyShader)
 
@@ -63,17 +92,17 @@ class createApp {
 		window.addEventListener('resize', this.onResize.bind(this))
 		// window.addEventListener('mousemove', this.onMouseMove.bind(this))
 
-		this.composer = new EffectComposer(this.renderer)
-	    this.composer.addPass(new RenderPass(this.scene, this.camera))
+		// this.composer = new EffectComposer(this.renderer)
+	 //    this.composer.addPass(new RenderPass(this.scene, this.camera))
 	 
 	    // Add shaders! Celebrate! 
 	    // const someShaderPass = new ShaderPass(SomeShader) 
 	    // this.composer.addPass(someShaderPass) 
 	 
 	    // And draw to the screen 
-	    const copyPass = new ShaderPass(CopyShader)
-	    copyPass.renderToScreen = true
-	    this.composer.addPass(copyPass)
+	    // const copyPass = new ShaderPass(CopyShader)
+	    // copyPass.renderToScreen = true
+	    // this.composer.addPass(copyPass)
 
 		this.animate()
 	}
@@ -94,11 +123,15 @@ class createApp {
 		this.camera.position.fromArray(this.controls.position);
 		this.camera.up.fromArray(this.controls.up);
 		this.camera.lookAt(this.target);
-
+		if (!this.anim) {
+			this.anim = true;
+			console.log(gsap)
+			gsap.TweenLite.to(this.line.material.uniforms.visibility,5, {value:1})
+		}
 		this.viewVector = new THREE.Vector3().subVectors( this.camera.position, this.mesh.getWorldPosition());
 		this.mesh.material.uniforms.viewVector.value = this.viewVector;
 
-		this.composer.render()
+		this.renderer.render(this.scene, this.camera)
 	}
 }
 
