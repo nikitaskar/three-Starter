ee5c7aa0946eb8037c6e9fe32a3f49b910540b91
nikitaskar <n.skargovskii@gmail.com>
2018-01-23 00:27:26 +0100

    don't know

--- app/createApp.js
+++ app/createApp.js
@@ -5,6 +5,8 @@ import path from 'path'
 import EffectComposer, { RenderPass, ShaderPass, CopyShader } from 'three-effectcomposer-es6'
 import meshLine from 'three.meshline'
 import gsap from 'gsap'
+
+
 class createApp {
 	constructor(opt) {
 		this.winWidth = window.innerWidth
@@ -38,7 +40,51 @@ class createApp {
 			let phi = Math.acos( -1 + ( 2 * i ) / 500 );
 			let theta = Math.sqrt(500 * Math.PI/3 ) * phi;
 
-			this.material = new THREE.ShaderMaterial({
+			
+		
+
+		    var v = new THREE.Vector3( 30 * Math.cos( theta ) * Math.sin( phi ), -30 * Math.cos( phi ), 30 * Math.sin( theta ) * Math.sin( phi ) );
+		    geometry.vertices.push( v );
+		}
+
+		var line = new meshLine.MeshLine();
+
+		line.setGeometry( geometry, function( p ) { return 1*Math.pow( 4 * p * ( 1 - p ), 0.5 ); } ); // makes width sinusoidal 
+
+		var material = new meshLine.MeshLineMaterial({
+			lineWidth: 1,
+			visibility: 0,
+			transparent: true,
+		});
+		this.line = new THREE.Mesh( line.geometry, material ); // this syntax could definitely be improved! 
+		this.scene.add( this.line );
+
+		
+
+		console.log(CopyShader)
+
+		this.renderer = new THREE.WebGLRenderer( {alpha: true} )
+		this.renderer.setSize(this.winWidth, this.winHeight)
+		document.body.appendChild(this.renderer.domElement)
+
+		window.addEventListener('resize', this.onResize.bind(this))
+		// window.addEventListener('mousemove', this.onMouseMove.bind(this))
+
+		// this.composer = new EffectComposer(this.renderer)
+	 //    this.composer.addPass(new RenderPass(this.scene, this.camera))
+	 
+	    // Add shaders! Celebrate! 
+	    // const someShaderPass = new ShaderPass(SomeShader) 
+	    // this.composer.addPass(someShaderPass) 
+	 
+	    // And draw to the screen 
+	    // console.log(CopyShader)
+	    // const copyPass = new ShaderPass(CopyShader)
+	    // copyPass.renderToScreen = true
+	    // this.composer.addPass(copyPass)
+
+
+	    this.material = new THREE.ShaderMaterial({
 			vertexShader:glsl((`uniform vec3 viewVector;
 				varying float intensity;
 				void main() {
@@ -57,52 +103,10 @@ class createApp {
 			uniforms: this.uniforms
 
 		});
-		this.geometry = new THREE.SphereGeometry(2,8,8)
+		this.geometry = new THREE.SphereGeometry(2,64,64)
 		this.mesh = new THREE.Mesh(this.geometry, this.material)
-		this.mesh.position.x = 30 * Math.cos( theta ) * Math.sin( phi );
-		this.mesh.position.y = -30 * Math.cos( phi );
-		this.mesh.position.z = 30 * Math.sin( theta ) * Math.sin( phi )
-		// this.scene.add(this.mesh)
-		
+		this.scene.add(this.mesh)
 
-		    var v = new THREE.Vector3( 30 * Math.cos( theta ) * Math.sin( phi ), -30 * Math.cos( phi ), 30 * Math.sin( theta ) * Math.sin( phi ) );
-		    geometry.vertices.push( v );
-		}
-
-		var line = new meshLine.MeshLine();
-
-		line.setGeometry( geometry, function( p ) { return 1*Math.pow( 4 * p * ( 1 - p ), 1 ); } ); // makes width sinusoidal 
-
-		var material = new meshLine.MeshLineMaterial({
-			lineWidth: 1,
-			visibility: 0,
-			transparent: true,
-		});
-		this.line = new THREE.Mesh( line.geometry, material ); // this syntax could definitely be improved! 
-		this.scene.add( this.line );
-
-		
-
-		console.log(CopyShader)
-
-		this.renderer = new THREE.WebGLRenderer( {antialias:true, alpha: true} )
-		this.renderer.setSize(this.winWidth, this.winHeight)
-		document.body.appendChild(this.renderer.domElement)
-
-		window.addEventListener('resize', this.onResize.bind(this))
-		// window.addEventListener('mousemove', this.onMouseMove.bind(this))
-
-		// this.composer = new EffectComposer(this.renderer)
-	 //    this.composer.addPass(new RenderPass(this.scene, this.camera))
-	 
-	    // Add shaders! Celebrate! 
-	    // const someShaderPass = new ShaderPass(SomeShader) 
-	    // this.composer.addPass(someShaderPass) 
-	 
-	    // And draw to the screen 
-	    // const copyPass = new ShaderPass(CopyShader)
-	    // copyPass.renderToScreen = true
-	    // this.composer.addPass(copyPass)
 
 		this.animate()
 	}
@@ -126,7 +130,7 @@ class createApp {
 		if (!this.anim) {
 			this.anim = true;
 			console.log(gsap)
-			gsap.TweenLite.to(this.line.material.uniforms.visibility,5, {value:1})
+			gsap.TweenLite.to(this.line.material.uniforms.visibility,5, {value:1, ease: Power4.easeInOut})
 		}
 		this.viewVector = new THREE.Vector3().subVectors( this.camera.position, this.mesh.getWorldPosition());
 		this.mesh.material.uniforms.viewVector.value = this.viewVector;
