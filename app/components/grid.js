28ca9f8e55d969c574f668d8adbe0aa9d940762f
Mac Pro 9P <macpro@Mac-Pro-de-Mac.local>
2018-01-23 18:46:16 +0100

    everything

--- app/components/grid.js
+++ app/components/grid.js
@@ -1,16 +1,67 @@
+import * as THREE from 'three'
+
 class Grid {
     constructor(opt) {
         this.instancedGeo = new THREE.InstancedBufferGeometry();
-        
+        this.count = opt.count
+        this.scene = opt.scene
+        this.coords = opt.coords
+        this.createBlueprint()
+        this.instanceBlueprint()
     }
 
     createBlueprint() {
-        this.blueprint = []
-        for (let i = 0; i < array.length; i++) {
-            let angle = Math.PI/180*(120*i)
-            this.blueprint.push(Math.cos(a), Math.sin(a), 0)
+        this.blueprint = this.coords
+       
+
+        console.log(this.blueprint)
+
+        let attribute =  new THREE.BufferAttribute(new Float32Array(this.blueprint),3)
+        this.instancedGeo.addAttribute('position', attribute)        
+    }
+
+    instanceBlueprint() {
+        let translation = new Float32Array(this.count*3)
+        let rotation = new Float32Array(this.count*4)
+
+        let translationIterator = 0;
+        let rotationIterator = 0;
+
+        let q = new THREE.Quaternion();
+
+        for (let i = 0; i < this.count; i++) {
+            translation[translationIterator++] = 0
+            translation[translationIterator++] = -i/390 
+            translation[translationIterator++] = 0
+            q.set(
+                (Math.random()-5)*2,
+                (Math.random()-5)*2,
+                (Math.random()-5)*2,
+                Math.random() * Math.PI 
+            )
+            q.normalize()
+            
+            rotation[rotationIterator++] = q.x
+            rotation[rotationIterator++] = q.w
+            rotation[rotationIterator++] = q.y
+            rotation[rotationIterator++] = q.z
+    
         }
 
-        
+        this.instancedGeo.addAttribute('translation', new THREE.InstancedBufferAttribute(translation,3,1))
+        this.instancedGeo.addAttribute('rotation', new THREE.InstancedBufferAttribute(rotation,4,1))
+
+        let material = new THREE.RawShaderMaterial(
+            {
+                vertexShader: document.getElementById('vertexShader').innerHTML,
+                fragmentShader: document.getElementById('fragmentShader').innerHTML,
+                side: THREE.DoubleSide,
+                wireframe: true,
+            }
+        )
+        this.grid = new THREE.Mesh(this.instancedGeo, material)
+        this.scene.add(this.grid)
     }
 }
+
+export default Grid
--- app/createApp.js
+++ app/createApp.js
@@ -6,58 +6,122 @@ import EffectComposer, { RenderPass, ShaderPass, CopyShader } from 'three-effect
 import meshLine from 'three.meshline'
 import gsap from 'gsap'
 
+import Grid from './components/grid.js'
 
 class createApp {
 	constructor(opt) {
 		this.winWidth = window.innerWidth
 		this.winHeight = window.innerHeight
 		this.winRatio = this.winWidth/this.winHeight
-		this.camera = new THREE.PerspectiveCamera( 75,this.winRatio, 0.1, 1000 )
+		this.camera = new THREE.PerspectiveCamera( 75,this.winRatio, 0.005, 1000 )
+		this.camera2 = new THREE.PerspectiveCamera( 75,this.winRatio, 0.005, 1000 )
+		this.camera.position.z = 1
+		this.camera2.position.z = 1
 		this.target = new THREE.Vector3()
-		this.camera.position.z = 65
-
+		console.log( )
 		this.scene = new THREE.Scene();
 
 		this.controls = orbitControls({
 			position : [0,0,0],
-			distance: 10,
+			distance: 0,
 			zoom: true,
 			rotateSpeed: 0.007,
 			damping: 0.05,
 		})
 
-		this.sphereMaterial = new THREE.MeshBasicMaterial({
-		})
+		// this.sphereMaterial = new THREE.MeshBasicMaterial({
+		// })
+
+		// this.sphereGeometry = new THREE.SphereGeometry(6,8,8)
+		// this.sphere = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial)
+
+		// this.scene.add(this.sphere)
+
+		this.renderer = new THREE.WebGLRenderer({antialias: true, alpha:true})
+		this.renderer.setSize(this.winWidth, this.winHeight)
+
+		document.body.appendChild(this.renderer.domElement)
+
+		window.addEventListener('resize', this.onResize.bind(this))
+		window.addEventListener('mousemove', this.onMouseMove.bind(this))
+		
+
+		let geo = new THREE.PlaneGeometry(2,2,28,28)
+		let material = new THREE.MeshBasicMaterial({wireframe:true})
+
+		let mesh = new THREE.Mesh(geo, material)
+		console.log(mesh)
+		mesh.rotation.z = Math.PI/4
+		this.scene.add(mesh)
+
+		console.log(this.grid)
+		
 
 
-		this.sphereGeometry = new THREE.SphereGeometry(6,8,8)
-		this.sphere = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial)
+		this.rawCoords = [
+			{
+				x:0,
+				y:0,
+			},
 
-		this.scene.add(this.sphere)
+			{	
+				x:this.winWidth/6,
+				y: this.winHeight/6
+			},
+			{	
+				x:0,
+				y: this.winHeight/3
+			},
+		]
 
-		this.renderer = new THREE.WebGLRenderer({antialias: true})
-
+		this.treatedCoords = []
+		
 		this.animate()
+		this.initCoords()
 	}
 
+	initCoords() {
+		for (let i = 0; i < this.rawCoords.length; i++) {
+			console.log(this.rawCoords)
+			let treatedCoordsX = ((this.rawCoords[i].x)/this.winWidth)*2-1
+			let treatedCoordsY = -((this.rawCoords[i].y)/this.winHeight)*2+1	
+			
+			this.newPos = new THREE.Vector3(treatedCoordsX, treatedCoordsY,-1).unproject(this.camera)
+			
+			this.treatedCoords.push(this.newPos.x, this.newPos.y, this.newPos.z)
+
+		}
+		this.grid = new Grid({count: 7, scene: this.scene, coords:this.treatedCoords})
+		console.log(this.treatedCoords)
+
+	}
+
+	onMouseMove(e) {
+		let mouseX = e.clientX
+		let mouseY = e.clientY
+		
+		this.mouseX = ((mouseX)/this.winWidth)*2-1
+		this.mouseY = -((mouseY)/this.winHeight)*2+1	
+	
+	}
 
 	onResize() {
 		this.winWidth = window.innerWidth
 		this.winHeight = window.innerHeight
 		this.winRatio = this.winWidth/this.winHeight
 		this.camera.aspect = this.winRatio;
-   		this.camera.updateProjectionMatrix();
+		this.camera.updateProjectionMatrix();	
 		this.renderer.setSize(this.winWidth, this.winHeight)
 	}
 
 	animate() {
 		requestAnimationFrame(this.animate.bind(this))
 		this.controls.update();
-		this.camera.position.fromArray(this.controls.position);
-		this.camera.up.fromArray(this.controls.up);
-		this.camera.lookAt(this.target);
+		this.camera2.position.fromArray(this.controls.position);
+		this.camera2.up.fromArray(this.controls.up);
 
-		this.renderer.render(this.scene, this.camera)
+		this.camera2.lookAt(this.target);
+		this.renderer.render(this.scene, this.camera2)
 	}
 }
 
