f1adc219a86cc6a526b25ab578acb3b4afde7942
nikitaskar <n.skargovskii@gmail.com>
2018-02-19 08:42:44 +0100

    folded paper

--- app/createApp.js
+++ app/createApp.js
@@ -11,51 +11,124 @@ class createApp {
 		this.winRatio = this.winWidth/this.winHeight
 		this.camera = new THREE.PerspectiveCamera( 75,this.winRatio, 0.1, 1000 )
 		this.target = new THREE.Vector3()
-		this.camera.position.z = 10
+		this.camera.position.z = 5
 
 		this.scene = new THREE.Scene();
 
 		this.controls = orbitControls({
 			position : [0,0,0],
 			distance: 10,
-			zoom: false,
+			zoom: true,
 			rotateSpeed: 0.007,
 			damping: 0.05,
 		})
-		this.uniforms = {
-			viewVector: {
-				type:"v3",
-				value:new THREE.Vector3( 0, 0, 0 ),
-			}
-		}
-		
-		this.material = new THREE.ShaderMaterial({
-			vertexShader:glsl((`uniform vec3 viewVector;
-				varying float intensity;
-				void main() {
-				  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position/5., 1.0 );
-				  vec3 actual_normal = vec3(modelMatrix * vec4(normal, 0.0));
-				  intensity = pow( dot(normalize(viewVector), actual_normal), 3.0 );
-				}`
-			)),
-			fragmentShader:glsl((`varying float intensity;
-				void main() {
-				  vec3 glow = vec3(246./255., 54./255., 81./255.) * intensity;
-				  gl_FragColor = vec4( glow, intensity );
-				}`
-			)),
-			transparent: true,
-			uniforms: this.uniforms
-
-		});
-		this.geometry = new THREE.SphereGeometry(2,64,64)
-		this.mesh = new THREE.Mesh(this.geometry, this.material)
-
-
-		this.scene.add(this.mesh)
 
 		console.log(CopyShader)
+		
+		this.rank = new Float32Array([
+			1,0,2,
+			2,0,1,
+			3,0,4,
+			4,0,1,
+			3,3,1,
+			3,3,4,
+			3,4,1
+		])
+		this.buffer = new Float32Array ([
+			0, 0,  0,
+			
+			0.5,  0.5,  0.,
+			0, 1,  0,
 
+			0, 1,  0,
+			0.5,  0.5,  0.,
+			1.,1.,0.,
+
+			1.,1.,0.,
+			0.5,  0.5,  0.,
+			1.,0,0,
+
+			1.,0,0,
+			0.5,  0.5,  0.,
+			0,0,0,
+
+			1,1,0,
+			1.5,.5,0,
+			1.5,1,0,
+			
+			1.5,.5,0,
+			1,1,0,
+			1,0,0,
+
+			
+			1.5,.5,0,
+			1,0,0,
+			1.5,0,0
+
+		] )
+
+		this.bufferFolded = new Float32Array ([
+			1,1,0,			
+			0.5,  0.5,  0.,
+			1,0,0,
+
+			1,0,0,
+			0.5,  0.5,  0.,
+			1.,1.,0.,
+
+			1.,1.,0.,
+			0.5,  0.5,  0.,
+			1.,0,0,
+
+			1.,0,0,
+			0.5,  0.5,  0.,
+			1,1,0,
+
+			1,1,0,
+			0.5,.5,0,
+			1,.5,0,
+			
+			0.5,.5,0,
+			1,1,0,
+			1,0,0,
+
+			
+			0.5,.5,0,
+			1,0,0,
+			1.,0.5,0
+
+		] )
+
+
+		this.bufferGeometry = new THREE.BufferGeometry()
+		this.bufferGeometry.addAttribute('position', new THREE.BufferAttribute(this.buffer,3))
+
+		this.bufferGeometry.addAttribute('folded', new THREE.BufferAttribute(this.bufferFolded,3))
+
+		this.bufferGeometry.addAttribute('rank', new THREE.BufferAttribute(this.rank,1))
+
+		this.uniforms= {
+			u_time:{
+				type:'f',
+				value:0,
+			}
+		}
+
+		this.material = new THREE.RawShaderMaterial(
+			{
+				vertexShader: document.getElementById('vert').innerHTML,
+				fragmentShader: document.getElementById('frag').innerHTML,
+				uniforms: this.uniforms,
+				side: THREE.DoubleSide,
+				wireframe: true,
+			}
+		)
+		this.mesh = new THREE.Mesh(this.bufferGeometry, this.material)
+		console.log(this.mesh)
+		this.scene.add(this.mesh)
+
+
+		
 		this.renderer = new THREE.WebGLRenderer( {antialias:true, alpha: true} )
 		this.renderer.setSize(this.winWidth, this.winHeight)
 		document.body.appendChild(this.renderer.domElement)
@@ -63,17 +136,7 @@ class createApp {
 		window.addEventListener('resize', this.onResize.bind(this))
 		// window.addEventListener('mousemove', this.onMouseMove.bind(this))
 
-		this.composer = new EffectComposer(this.renderer)
-	    this.composer.addPass(new RenderPass(this.scene, this.camera))
-	 
-	    // Add shaders! Celebrate! 
-	    // const someShaderPass = new ShaderPass(SomeShader) 
-	    // this.composer.addPass(someShaderPass) 
-	 
-	    // And draw to the screen 
-	    const copyPass = new ShaderPass(CopyShader)
-	    copyPass.renderToScreen = true
-	    this.composer.addPass(copyPass)
+	
 
 		this.animate()
 	}
@@ -92,13 +155,12 @@ class createApp {
 		requestAnimationFrame(this.animate.bind(this))
 		this.controls.update();
 		this.camera.position.fromArray(this.controls.position);
-		this.camera.up.fromArray(this.controls.up);
-		this.camera.lookAt(this.target);
-
-		this.viewVector = new THREE.Vector3().subVectors( this.camera.position, this.mesh.getWorldPosition());
-		this.mesh.material.uniforms.viewVector.value = this.viewVector;
-
-		this.composer.render()
+ 		this.camera.up.fromArray(this.controls.up);
+ 		this.camera.lookAt(this.target)
+ 	
+		this.uniforms.u_time.value += .01
+		console.log(this.uniforms.u_time.value)
+		this.renderer.render(this.scene, this.camera)
 	}
 }
 
